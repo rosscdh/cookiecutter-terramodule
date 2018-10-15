dee4b394844f434c8f674af1e339df266bf1c98f
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2018-10-15 20:19:07 +0200

    Base

---
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.tf
@@ -0,0 +1,9 @@
+#
+# Start Putting your Terraform definitions here
+#
+# ie.
+# resource "aws_launch_configuration" "cluster" {
+#   image_id      = "${data.aws_ami.ubuntu.id}"
+#   instance_type = "${var.instance_type}"
+# }
+#
